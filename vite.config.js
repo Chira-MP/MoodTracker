eda2fd91c862100b5e3c62f895edfd76012183b2
Paula <paula.chira@emanuel.ro>
2026-01-21 20:56:52 +0200

    Fix deployment path

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa' // <--- Am adăugat asta
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import { VitePWA } from "vite-plugin-pwa" // <--- Am adăugat asta
 
 export default defineConfig({
-  base: '/',
+  base: "/MoodTracker/",
 
   plugins: [
     vue(),
     vueDevTools(),
     // Configurarea PWA de mai jos:
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       devOptions: {
         enabled: true // Permite testarea PWA și în timpul dezvoltării
       },
       manifest: {
-        name: 'MoodTracker App',
-        short_name: 'MoodTracker',
-        description: 'Monitorizează-ți starea de spirit zilnic',
-        theme_color: '#42b883',
+        name: "MoodTracker App",
+        short_name: "MoodTracker",
+        description: "Monitorizează-ți starea de spirit zilnic",
+        theme_color: "#42b883",
         icons: [
           {
-            src: 'favicon.ico', // Folosim favicon-ul tău momentan ca să nu dea eroare
-            sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/x-icon'
+            src: "favicon.ico", // Folosim favicon-ul tău momentan ca să nu dea eroare
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
           }
         ]
       }
@@ -34,11 +34,11 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
 
   define: {
     __VUE_OPTIONS_API__: false
-  },
+  }
 })
